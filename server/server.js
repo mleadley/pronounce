a7fa607c6320e19a79e8c2c10acb05a4362ad467
Michael Leadley <leadley75@gmail.com>
2017-10-28 15:14:52 +0100

    Add outline of words_controller

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const app = express()
+const wordsRouter = require("./controllers/words_controller")
 
 app.use(express.static(__dirname + "/../client/build"))
+
+app.use("/api/words", wordsRouter)
 
 app.listen(3000)
