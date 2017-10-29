141388104db0b8d43f8de19ec7840873cf03f15f
Michael Leadley <leadley75@gmail.com>
2017-10-29 09:58:27 +0000

    Complete RESTful routes in words_controller

--- server/controllers/words_controller.js
+++ server/controllers/words_controller.js
@@ -1,30 +1,43 @@
 const express = require("express")
 const router = express.Router()
+const wordsQuery = require("../db/words_query")
 
 // INDEX
 var indexRoute = function (req, res) {
-
+  wordsQuery.all(function (data) {
+    res.json(data)
+  })
 }
 
 router.get("/", indexRoute)
 
 // SHOW
 var showRoute = function (req, res) {
-
+  var query = { word: req.params.word }
+  wordsQuery.find(query, function () {
+    res.json(data)
+  })
 }
 
 router.get("/:word", showRoute)
 
-// NEW
+// CREATE
 var newRoute = function (req, res) {
-
+  var wordData = req.body
+  wordsQuery.save(wordData, wordData, function () {
+    res.json(data)
+  })
 }
 
 router.post("/", newRoute)
 
 // UPDATE
 var updateRoute = function (req, res) {
-
+  var query = { word: req.params.word }
+  var wordData = req.body
+  wordsQuery.save(query, wordData, function () {
+    res.json(data)
+  })
 }
 
 router.post("/:word", updateRoute)
@@ -32,7 +45,10 @@ router.put("/:word", updateRoute)
 
 // DELETE
 var deleteRoute = function (req, res) {
-
+  var query = { word: req.params.word }
+  wordsQuery.delete(query, function () {
+    res.json(data)
+  })
 }
 
 router.post("/:word/delete", deleteRoute)
