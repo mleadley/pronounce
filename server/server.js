36edc8ce6f6d5abd7921448bd808bcba0dcf0a17
Michael Leadley <leadley75@gmail.com>
2017-10-29 09:50:31 +0000

    Add body-parser functions to server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express")
 const app = express()
 const wordsRouter = require("./controllers/words_controller")
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(__dirname + "/../client/build"))
 
 app.use("/api/words", wordsRouter)
