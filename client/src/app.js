9e5071dd03d503f4fdd131b2299a7f80260b1183
Daniel lara <daniellaraedinburgh@hotmail.com>
2017-10-30 16:34:32 +0000

    prepare for merge

--- client/src/app.js
+++ client/src/app.js
@@ -2,9 +2,7 @@ var renderLandingPage = require("./views/landing_page/landing_page.js")
 var renderWordPage = require("./views/word_page/word_page.js")
 
 var app = function () {
-  // renderLandingPage()
-  renderWordPage()
-
+  renderLandingPage()
 }
 
 
