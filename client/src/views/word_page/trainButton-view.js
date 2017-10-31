3581a2e31f7f46a714a407aa5486627f78efec85
David Sturgeon <davidsturgeon500@hotmail.com>
2017-10-31 18:00:55 +0000

    add score bar visual

--- client/src/views/word_page/trainButton-view.js
+++ client/src/views/word_page/trainButton-view.js
@@ -2,7 +2,7 @@ var startRecognition = require("../../helpers/web_speech.js")
 
 var renderTrainButton = function (currentWord) {
   var recognition = startRecognition()
-  var diagnostic = document.querySelector('p#score-visualisation');
+  var diagnostic = document.querySelector('#score-visualisation');
 
   recognition.onresult = function(event) {
     var last = event.results.length - 1;
@@ -11,13 +11,27 @@ var renderTrainButton = function (currentWord) {
 
     var wordMatch = currentWord === recognisedWord
 
+    conf = event.results[0][0].confidence * 100;
 
-    diagnostic.innerHTML = 'Result received: ' + recognisedWord + "<br>" +
-                           "Confidence: " + event.results[0][0].confidence + "<br>";
+    // diagnostic.innerHTML = 'Result received: ' + recognisedWord + "<br>" +
+    //                        "Confidence: " + conf + "<br>";
 
     if (wordMatch) {
-      diagnostic.innerHTML += "WORD MATCH!!!"
-    }
+      // diagnostic.innerHTML += "WORD MATCH!!!"
+      function move() {
+        var width = 0;
+        var id = setInterval(frame, 10);
+        function frame() {
+          if (width >= conf) {
+            clearInterval(id);
+          } else {
+            width++;
+            diagnostic.style.width = width + '%';
+            diagnostic.innerHTML = "CORRECT " + width * 1  + '%';
+          }
+        }
+      }
+    } move()
 
     console.log('Result received: ' + recognisedWord)
     console.log('Confidence: ' + event.results[0][0].confidence);
--- client/src/views/word_page/word_page.js
+++ client/src/views/word_page/word_page.js
@@ -22,7 +22,7 @@ return phoneticField
 }
 
 var renderScoreVisualisation = function (visualisation){
-var scoreVisualisation = document.createElement("p")
+var scoreVisualisation = document.createElement("div")
 scoreVisualisation.id = "score-visualisation"
 scoreVisualisation.innerText = visualisation
 scoreVisualisation.label = "score visualisation"
