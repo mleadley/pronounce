0b1b874ef421d98eb860dd031ca569041df90727
David Sturgeon <davidsturgeon500@hotmail.com>
2017-10-31 11:36:51 +0000

    update web_speech file

--- client/src/helpers/web_speech.js
+++ client/src/helpers/web_speech.js
@@ -1,37 +1,39 @@
 // var webkitSpeechRecognition = webkitSpeechRecognition || null
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 
-if (SpeechRecognition) {
-  var recognition = new SpeechRecognition();
+var startRecognition = function() {
+  if (SpeechRecognition) {
+    var recognition = new SpeechRecognition();
 
-  recognition.lang = 'en-UK';
-  recognition.interimResults = false;
-  recognition.maxAlternatives = 1;
+    recognition.lang = 'en-UK';
+    recognition.interimResults = false;
+    recognition.maxAlternatives = 1;
 
-  var diagnostic = document.querySelector('#score-visualisation');
+    var diagnostic = document.querySelector('p#score-visualisation');
 
-  recognition.onresult = function(event) {
-    var last = event.results.length - 1;
-    var recognisedWord = event.results[last][0].transcript;
+    recognition.onresult = function(event) {
+      var last = event.results.length - 1;
+      var recognisedWord = event.results[last][0].transcript;
+      diagnostic.textContent = 'Result received: ' + recognisedWord + "\n" +
+                             "Confidence: " + event.results[0][0].confidence;
 
-    diagnostic.innerHTML = 'Result received: ' + recognisedWord + "\n" +
-                           "Confidence: " + event.results[0][0].confidence;
+      console.log('Result received: ' + recognisedWord)
+      console.log('Confidence: ' + event.results[0][0].confidence);
+    }
 
-    console.log('Result received: ' + recognisedWord)
-    console.log('Confidence: ' + event.results[0][0].confidence);
+    recognition.onspeechend = function() {
+      recognition.stop();
+    }
+
+    recognition.onnomatch = function(event) {
+      console.log("I didn't recognise that word.");
+    }
+
+    recognition.onerror = function(event) {
+      console.log('Error occurred in recognition: ' + event.error);
+    }
+    return recognition;
   }
-
-  recognition.onspeechend = function() {
-    recognition.stop();
-  }
-
-  recognition.onnomatch = function(event) {
-    console.log("I didn't recognise that word.");
-  }
-
-  recognition.onerror = function(event) {
-    console.log('Error occurred in recognition: ' + event.error);
-  }
-
-  module.exports = recognition
 }
+
+module.exports = startRecognition
