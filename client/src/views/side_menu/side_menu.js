9593ab0ae2fee1c100cde727a64137ef7e7acd1a
Michael Leadley <leadley75@gmail.com>
2017-11-01 09:39:44 +0000

    Hamburger button now activates menu

--- client/src/views/side_menu/side_menu.js
+++ client/src/views/side_menu/side_menu.js
@@ -1,24 +1,34 @@
 var requestHelper = require("../../helpers/request_helper")
 
 
-var createHtml = function (wordsArray) {
+var renderSideMenu = function (wordsArray) {
   var nav = document.querySelector("nav#side-menu")
 
   var yourWords = document.createElement("ul")
-  yourWords.classList.add("side-menu")
-  yourWords.id = "side-menu-ul"
+  yourWords.classList.add("hidden")
 
   requestHelper.get("http://localhost:3000/api/words", function (words) {
     words.forEach(function (words) {
       var savedWord = document.createElement("li")
       savedWord.innerText = words.word
-      savedWord.classList.add("side-menu-li")
       yourWords.appendChild(savedWord)
     })
   })
 
   nav.appendChild(yourWords)
+
+  var hamburgerButton = document.querySelector(".hamburger")
+
+  hamburgerButton.addEventListener("click", function () {
+    if(yourWords.classList.contains("hidden")){
+      hamburgerButton.classList.add("is-active")
+      yourWords.classList.remove("hidden")
+    } else {
+      hamburgerButton.classList.remove("is-active")
+      yourWords.classList.add("hidden")
+    }
+  })
 }
 
 
-module.exports = createHtml
+module.exports = renderSideMenu
