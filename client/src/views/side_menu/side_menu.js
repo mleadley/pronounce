cbb30cd11dcea6948d31dacfa9ec583a23d01cdc
Michael Leadley <leadley75@gmail.com>
2017-11-01 13:14:41 +0000

    Add desired functionality to all menu links

--- client/src/views/side_menu/side_menu.js
+++ client/src/views/side_menu/side_menu.js
@@ -1,8 +1,38 @@
 var requestHelper = require("../../helpers/request_helper")
+var renderLandingPage = require("../landing_page/landing_page.js")
+var renderWordPage = require("../word_page/word_page.js")
+var clearStartChain = require("../../helpers/clear_start_chain.js")
 
 var renderSideMenu = function (wordsArray) {
   var nav = document.querySelector("nav#side-menu")
   var completedList = nav.querySelector("#completed ul")
+  var newSearchButton = document.querySelector("#new-search")
+  var hamburgerButton = document.querySelector(".hamburger")
+
+  var toggleMenu = function () {
+    if( nav.classList.contains("open") ) {
+      nav.classList.remove("open")
+      hamburgerButton.classList.remove("is-active")
+    } else {
+      nav.classList.add("open")
+      hamburgerButton.classList.add("is-active")
+    }
+  }
+
+  var onWordClick = function (event) {
+    event.preventDefault()
+    var chosenWord = event.target.innerText
+    toggleMenu()
+    clearStartChain()
+    renderWordPage(chosenWord)
+  }
+
+  var onNewSearchClick = function (event) {
+    event.preventDefault()
+    toggleMenu()
+    clearStartChain()
+    renderLandingPage()
+  }
 
   requestHelper.get("http://localhost:3000/api/words", function (words) {
     words.forEach(function (word) {
@@ -11,27 +41,16 @@ var renderSideMenu = function (wordsArray) {
 
       savedWordLink.innerText = word.word
       savedWordLink.href = ""
-      savedWordLink.addEventListener("click", function (event) {
-        event.preventDefault()
-        console.log(this.innerText)
-      })
+      savedWordLink.addEventListener("click", onWordClick)
 
       savedWord.appendChild(savedWordLink)
       completedList.appendChild(savedWord)
     })
   })
 
-  var hamburgerButton = document.querySelector(".hamburger")
+  newSearchButton.addEventListener("click", onNewSearchClick)
 
-  hamburgerButton.addEventListener("click", function () {
-    if(nav.classList.contains("open")) {
-      nav.classList.remove("open")
-      hamburgerButton.classList.remove("is-active")
-    } else {
-      nav.classList.add("open")
-      hamburgerButton.classList.add("is-active")
-    }
-  })
+  hamburgerButton.addEventListener("click", toggleMenu)
 }
 
 module.exports = renderSideMenu
