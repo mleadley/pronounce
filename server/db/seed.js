4b646434cfa57ff282b06e5d069fc04e38fd7478
Kyle Miller <kylemiller995@gmail.com>
2017-11-01 19:51:01 +0000

    add seed file

--- server/db/seed.js
+++ server/db/seed.js
@@ -4,7 +4,19 @@ db.dropDatabase()
 
 
 db.words.insert([
-  {word: "word"},
-  {word: "beach"},
-  {word: "constantly"}
+  {word: "word", completed: true},
+  {word: "beach", completed: true},
+  {word: "constantly", completed: false}
+  {word: "bottle", completed: true}
+  {word: "cup", completed: true}
+  {word: "jacket", completed: true}
+  {word: "television", completed: true}
+  {word: "water", completed: false}
+  {word: "paper", completed: false}
+  {word: "crip", completed: true}
+  {word: "plastic", completed: true}
+  {word: "coffee", completed: true}
+  {word: "kyle", completed: false}
+  {word: "pen", completed: true}
+  {word: "charge", completed: false}
 ])
