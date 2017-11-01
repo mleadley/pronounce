c0e30affa209fc95bedee7c8a3ca138e7fc9e163
Kyle Miller <kylemiller995@gmail.com>
2017-11-01 16:53:39 +0000

    hotfix default difficulty value

--- client/src/views/side_menu/settingButtons.js
+++ client/src/views/side_menu/settingButtons.js
@@ -8,6 +8,9 @@ handleDifficultySelector.handleChange = function (){
   var selector = document.getElementById("difficulty-options")
   selector.addEventListener("change", function(){
     var selectorValue = selector.value
+    if (selectorValue === null) {
+      selectorValue === "2"
+    }
     console.log(selectorValue)
     localStorage.setItem("difficultyValue", selectorValue)
   })
